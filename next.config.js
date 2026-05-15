d194dfb5417f6938880b6615703e345da151c4f5
karkyon <your-email@example.com>
2026-05-15 18:46:18 +0900

    更新

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.1.11'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['192.168.1.11:3033'],
+    },
+  },
 }
 
 module.exports = nextConfig
